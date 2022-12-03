60596410817563766f65bf5d0d01e233542b3ba5
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-12-04 00:06:31 +0100

    Correction test of CategoryService and CategoryController

--- src/test/java/guru/springframework/services/CategoryServiceTest.java
+++ src/test/java/guru/springframework/services/CategoryServiceTest.java
@@ -28,43 +28,19 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 class CategoryServiceTest {
 
-    public static final Long ID=2l;
-    public static final String NAME="Jimmy";
+
+    public static final Long ID = 2L;
+    public static final String NAME = "Jimmy";
     CategoryService categoryService;
 
     @Mock
     CategoryRepository categoryRepository;
 
-    @InjectMocks
-    CategoryController categoryController;
-
-    MockMvc mockMvc;
-
     @BeforeEach
     public void setUp() throws Exception {
-        MockitoAnnotations.openMocks(this);
-        categoryService = new CategoryServiceImpl(CategoryMapper.INSTANCE, categoryRepository);
+            MockitoAnnotations.openMocks(this);
 
-        mockMvc = MockMvcBuilders.standaloneSetup(categoryController).build();
-    }
-
-    @Test
-    public void testListCategories() throws Exception{
-        CategoryDTO category1 = new CategoryDTO();
-        category1.setId(1l);
-        category1.setName(NAME);
-
-        CategoryDTO category2 = new CategoryDTO();
-        category2.setId(2l);
-        category2.setName("Bob");
-
-        List<CategoryDTO> categories = Arrays.asList(category1,category2);
-
-        when(categoryService.getAllCategories()).thenReturn(categories);
-
-        mockMvc.perform(get("/api/v1/categories/").contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.categories", hasSize(2)));
+            categoryService = new CategoryServiceImpl(CategoryMapper.INSTANCE, categoryRepository);
     }
 
     @Test
