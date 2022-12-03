b19a54737a40901bf16727b964771169b606f622
mateuszlipowski22 <mateuszlipowski22@gmail.com>
2022-12-03 23:54:36 +0100

    Adding CategoryController with method getAllCategories and getCategoryByName

--- src/test/java/guru/springframework/services/CategoryServiceTest.java
+++ src/test/java/guru/springframework/services/CategoryServiceTest.java
@@ -2,20 +2,29 @@ package guru.springframework.services;
 
 import guru.springframework.api.v1.mapper.CategoryMapper;
 import guru.springframework.api.v1.model.CategoryDTO;
+import guru.springframework.controllers.v1.CategoryController;
 import guru.springframework.domain.Category;
 import guru.springframework.repository.CategoryRepository;
 import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class CategoryServiceTest {
 
@@ -26,10 +35,36 @@ class CategoryServiceTest {
     @Mock
     CategoryRepository categoryRepository;
 
+    @InjectMocks
+    CategoryController categoryController;
+
+    MockMvc mockMvc;
+
     @BeforeEach
     public void setUp() throws Exception {
         MockitoAnnotations.openMocks(this);
         categoryService = new CategoryServiceImpl(CategoryMapper.INSTANCE, categoryRepository);
+
+        mockMvc = MockMvcBuilders.standaloneSetup(categoryController).build();
+    }
+
+    @Test
+    public void testListCategories() throws Exception{
+        CategoryDTO category1 = new CategoryDTO();
+        category1.setId(1l);
+        category1.setName(NAME);
+
+        CategoryDTO category2 = new CategoryDTO();
+        category2.setId(2l);
+        category2.setName("Bob");
+
+        List<CategoryDTO> categories = Arrays.asList(category1,category2);
+
+        when(categoryService.getAllCategories()).thenReturn(categories);
+
+        mockMvc.perform(get("/api/v1/categories/").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.categories", hasSize(2)));
     }
 
     @Test
